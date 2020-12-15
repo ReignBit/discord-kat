8834551c58e15587117c2aeb572155aa8f817d8f
Bit <49931445+ReignBit@users.noreply.github.com>
2020-12-15 02:04:51 +0000

    Remove old failing code

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,10 @@ from bot.utils.models import KatGuild, KatMember, KatUser
 
 @pytest.fixture(scope="session")
 def setup():
-    options = ""
 
     sql_pass = os.environ.get("sql-pass")
     sql_user = os.environ.get("sql-user")
     sql_db = os.environ.get("sql-db")
-
-    options = json.loads(options)['sql']
 
     sql = SqlEngine(local=False, standalone=True, options={
                         'hostname': "reign-network.co.uk", "user": sql_user, "password": sql_pass, "db": sql_db})
