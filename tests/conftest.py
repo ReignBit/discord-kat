c79c212fdf874cb1ff77a3cdca959f77c18e9a3f
Bit <49931445+ReignBit@users.noreply.github.com>
2020-12-15 01:58:45 +0000

    Fix test-unit env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import pytest
 
@@ -9,13 +10,15 @@ from bot.utils.models import KatGuild, KatMember, KatUser
 @pytest.fixture(scope="session")
 def setup():
     options = ""
-    with open('config/config.json', 'r') as f:
-        options = f.read()
+
+    sql_pass = os.environ.get("sql-pass")
+    sql_user = os.environ.get("sql-user")
+    sql_db = os.environ.get("sql-db")
 
     options = json.loads(options)['sql']
 
     sql = SqlEngine(local=False, standalone=True, options={
-                        'hostname': "reign-network.co.uk", "user": "kat_user", "password": options['password'], "db": "yumi_database"})
+                        'hostname': "reign-network.co.uk", "user": sql_user, "password": sql_pass, "db": sql_db})
     sql.create_sql_session()
 
     return sql
