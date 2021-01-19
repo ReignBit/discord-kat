3061af90aeabcd9aa670b3e8c90af1c9a7a476b3
Bit <49931445+ReignBit@users.noreply.github.com>
2021-01-19 23:50:50 +0000

    Applying formatting convensions. miscellaneous fixes.

--- bot/cogs/translate.py
+++ bot/cogs/translate.py
@@ -11,18 +11,18 @@ from bot.utils.extensions import KatCog, read_resource
 class Translate(KatCog):
     def __init__(self, bot):
         super().__init__(bot)
-        # make sure we only have one translator. Only really using this for Reign for now, in the future
-        # we may want to have this be a dict and have some sort of TranslatorManager or smth.
         self.tranlator = None
 
         # list of all languages that have an ISO code (2-char)
         self.lang_list = {}
 
         # retrieve said list and turn it into a dict using json.load()
-        self.lang_list = read_resource('languages.json')
+        self.lang_list = read_resource("languages.json")
 
     async def _translate(self, from_lang, to_lang, msg):
-        self.tranlator = Translator(to_lang=to_lang, from_lang=from_lang, provider="mymemory")
+        self.tranlator = Translator(
+            to_lang=to_lang, from_lang=from_lang, provider="mymemory"
+        )
         translation = self.tranlator.translate(msg)
         # self.log.debug(type(translation))
         return html.unescape(translation)
@@ -35,21 +35,26 @@ class Translate(KatCog):
 
         if re.search("[\u3040-\u30ff]", message.content) is not None:
             result = await self._translate("ja", "en", message.content)
-            embed = discord.Embed(colour=discord.Colour(0x2b2b2b), description=result)
-            embed.set_author(name="{} Auto Translate".format(message.author.display_name), icon_url=message.author.avatar_url)
-            embed.set_footer(text="⚠️| Powered by opensource translations. May not be accurate.")
-            #embed.set_footer(text="Options to disable auto translation are WIP.")
+            embed = discord.Embed(colour=discord.Colour(0x2B2B2B), description=result)
+            embed.set_author(
+                name="{} Auto Translate".format(message.author.display_name),
+                icon_url=message.author.avatar_url,
+            )
+            embed.set_footer(
+                text="⚠️| Powered by opensource translations. May not be accurate."
+            )
+            # embed.set_footer(text="Options to disable auto translation are WIP.")
             await message.channel.send(embed=embed)
 
-    @commands.command(aliases=['tlast', 'translateuser', 'tuser'])
-    async def translatelast(self, ctx, user: discord.User, lang='en'):
+    @commands.command(aliases=["tlast", "translateuser", "tuser"])
+    async def translatelast(self, ctx, user: discord.User, lang="en"):
         """Translates a user's last message in the current channel"""
         # from_lang : to_lang
         if ":" in lang:
-            from_lang = lang.split(':')[0]
-            to_lang = lang.split(':')[1]
+            from_lang = lang.split(":")[0]
+            to_lang = lang.split(":")[1]
             if len(to_lang) != 2:
-                to_lang = 'en'
+                to_lang = "en"
         else:
             from_lang = "ja"
             to_lang = lang
@@ -57,67 +62,82 @@ class Translate(KatCog):
         msgs = await ctx.channel.history(limit=100).flatten()
         for message in msgs:
             if message.author == user:
-                result = await self._translate(from_lang, to_lang, message.clean_content)
-                embed = discord.Embed(colour=discord.Colour(0x2b2b2b), description=result)
-                embed.set_author(name="{}".format(message.author.display_name), icon_url=message.author.avatar_url)
+                result = await self._translate(
+                    from_lang, to_lang, message.clean_content
+                )
+                embed = discord.Embed(
+                    colour=discord.Colour(0x2B2B2B), description=result
+                )
+                embed.set_author(
+                    name="{}".format(message.author.display_name),
+                    icon_url=message.author.avatar_url,
+                )
                 await ctx.send(embed=embed)
                 return
 
-        embed = discord.Embed(colour=discord.Colour(0x2b2b2b), description="Could not find {}'s last message. Could be older than 100 messages!".format(user.display_name))
+        embed = discord.Embed(
+            colour=discord.Colour(0x2B2B2B),
+            description="Could not find {}'s last message. Could be older than 100 messages!".format(
+                user.display_name
+            ),
+        )
         await ctx.send(embed=embed)
 
-    @commands.command(aliases=['tmsg'])
-    async def translatemsg(self, ctx, message_id: int, lang='en'):
+    @commands.command(aliases=["tmsg"])
+    async def translatemsg(self, ctx, message_id: int, lang="en"):
         """Translates a message by message id"""
         # from_lang : to_lang
         if ":" in lang:
-            from_lang = lang.split(':')[0]
-            to_lang = lang.split(':')[1]
-            if len(to_lang) is not 2:
-                to_lang = 'en'
+            from_lang = lang.split(":")[0]
+            to_lang = lang.split(":")[1]
+            if len(to_lang) != 2:
+                to_lang = "en"
         else:
             from_lang = "ja"
             to_lang = lang
 
         try:
             msgs = await ctx.channel.fetch_message(message_id)
-            #self.log.debug(msgs)
+            # self.log.debug(msgs)
             result = await self._translate(from_lang, to_lang, msgs.clean_content)
         except Exception as e:
             self.log.debug(e)
             await ctx.send("Unable to find message. Could be deleted.")
             return
 
-        embed = discord.Embed(colour=discord.Colour(0x2b2b2b), description=result)
-        embed.set_author(name="{}".format(msgs.author.name), icon_url=msgs.author.avatar_url)
+        embed = discord.Embed(colour=discord.Colour(0x2B2B2B), description=result)
+        embed.set_author(
+            name="{}".format(msgs.author.name), icon_url=msgs.author.avatar_url
+        )
         await ctx.send(embed=embed)
 
-
     @commands.command()
-    async def translate(self, ctx, lang='en'):
-        """Attempts to translate the last sent message. If no language code is given then defaults to English."""
+    async def translate(self, ctx, lang="en"):
+        """Attempts to translate the last sent message.
+        If no language code is given then defaults to English.
+        """
 
         # $translate list
         if lang == "list":
             _ = "Language Codes for Translation (Some listed languages may not be supported)"
             # return a fancy string with nice formatted list of languages.
             for k, v in self.lang_list.items():
-                _ += "`{}` : {}\n".format(k,v)
+                _ += "`{}` : {}\n".format(k, v)
             # send the list to authors pm box
             await ctx.author.send("```apache\n{}```".format(_))
             return
 
         # from_lang : to_lang
         if ":" in lang:
-            from_lang = lang.split(':')[0]
-            to_lang = lang.split(':')[1]
-            if len(to_lang) is not 2:
-                to_lang = 'en'
+            from_lang = lang.split(":")[0]
+            to_lang = lang.split(":")[1]
+            if len(to_lang) != 2:
+                to_lang = "en"
         else:
             from_lang = "ja"
             to_lang = lang
 
-        self.log.debug(to_lang + ':' + from_lang)
+        self.log.debug(to_lang + ":" + from_lang)
         msgs = await ctx.channel.history(limit=2).flatten()
 
         # if the message includes an embed then cancel the translation.
@@ -127,19 +147,29 @@ class Translate(KatCog):
             async with ctx.channel.typing():
                 # Attempt to translate. Unsure what errors, if any, are thrown by Translator
 
-                result = await self._translate(from_lang, to_lang, msgs[1].clean_content)
+                result = await self._translate(
+                    from_lang, to_lang, msgs[1].clean_content
+                )
                 # trying to translate from the same language (en-en)
                 if result == b"PLEASE SELECT TWO DISTINCT LANGUAGES":
                     await ctx.send("Already in English!")
                     return
                 # was given an invalid language to translate into to / from
                 if b"INVALID TARGET LANGUAGE" in result:
-                    await ctx.send("Invalid language! For a full list of available languages, do `$translate list`")
+                    await ctx.send(
+                        "Invalid language! For a full list of available languages, "
+                        "do `$translate list`"
+                    )
                     return
 
                 # create the embed to show fancy results to user.
-                embed = discord.Embed(colour=discord.Colour(0x2b2b2b), description="{}".format(result))
-                embed.set_author(name="{}".format(msgs[1].author.display_name), icon_url=msgs[1].author.avatar_url)
+                embed = discord.Embed(
+                    colour=discord.Colour(0x2B2B2B), description="{}".format(result)
+                )
+                embed.set_author(
+                    name="{}".format(msgs[1].author.display_name),
+                    icon_url=msgs[1].author.avatar_url,
+                )
 
             # delete the command message
             await msgs[0].delete()
@@ -150,40 +180,34 @@ class Translate(KatCog):
             # message had an embed. aborted.
             await ctx.send("Unable to translate that message :(")
 
-
-
-    @commands.command(aliases=['ttext'])
-    async def translatetext(self, ctx, lang='en'):
+    @commands.command(aliases=["ttext"])
+    async def translatetext(self, ctx, lang="en"):
         """Translate text in the message that was sent by the author"""
 
         # Get rid of extra text from msg
         msg = ctx.message.clean_content
         if "$ttext" in msg:
-            msg = msg.replace("$ttext","")
+            msg = msg.replace("$ttext", "")
         else:
-            msg = msg.replace("$translatetext","")
+            msg = msg.replace("$translatetext", "")
         await ctx.send(msg)
-
 
         # from_lang : to_lang
         if ":" in lang:
-            from_lang = lang.split(':')[0]
-            to_lang = lang.split(':')[1]
-            if len(to_lang) is not 2:
-                to_lang = 'en'
+            from_lang = lang.split(":")[0]
+            to_lang = lang.split(":")[1]
+            if len(to_lang) != 2:
+                to_lang = "en"
         else:
             from_lang = "ja"
             to_lang = lang
 
         try:
-            #self.log.debug(msgs)
+            # self.log.debug(msgs)
             await self._translate(from_lang, to_lang, msg.clean_content)
         except Exception as e:
             self.log.debug(e)
             await ctx.send("Unable to find message. Could be deleted.")
-
-
-
 
 
 def setup(bot):
